a26c26c5648d4f7b85c94d40e88472ff584eb627
Charles Marttinen <mail@charlesmarttinen.ca>
2018-12-12 15:39:38 -0500

    Update index button styles

--- src/components/IndexButton.js
+++ src/components/IndexButton.js
@@ -5,16 +5,38 @@ const base = css`
     pointer-events: auto;
     border: none;
     color: white;
-    background: hsla(0, 0%, 21%, 0.6);
-    margin: 0 0.5em;
+    background: hsla(0, 0%, 0%, 0.8);
+    width: 1em;
+    height: 1em;
+    align-self: center;
+    margin: 0.75em;
+    border-radius: 50%;
+    padding: 0em;
+    font-family: serif;
+    box-shadow: 0 0 2px hsla(0, 0%, 100%, 1);
 `;
 
 const current = css`
     background: black;
+    min-width: 2em;
+    min-height: 2em;
+    margin: 0 0.25em;
 `;
 
 const nextOrPrevious = css`
-    background: #555;
+    width: 1.4em;
+    height: 1.4em;
+    margin: 0 0.65em;
+
+    & + &::after {
+        content: '';
+        display: block;
+        height: 0.75em;
+        width: 0.75em;
+        transform: translateX(-1em);
+        background: black;
+        border-radius: 100%;
+    }
 `;
 
 const IndexButton = props => {
@@ -27,9 +49,10 @@ const IndexButton = props => {
                 {[current]: isCurrent},
                 {[nextOrPrevious]: isNext || isPrevious}
             )}
+            title={index}
             {...innerProps}
         >
-            {index}
+            {isCurrent && index}
         </button>
     );
 };
--- src/components/IndexButtonsContainer.js
+++ src/components/IndexButtonsContainer.js
@@ -3,12 +3,16 @@ import {css} from 'emotion';
 
 const className = css`
     position: absolute;
-    bottom: 1em;
+    bottom: 0;
+    padding: 1em;
     left: 50%;
     transform: translateX(-50%);
     display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
     z-index: 3;
     pointer-events: none;
+    width: 100%;
 `;
 
 const IndexButtonsContainer = props => {
